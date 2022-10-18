c6857527e886641c7c2804a16dc94fb61b9abd76
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-10-18 21:22:46 +0800

    basic server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
+const categories = require('./data/categories.json')
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+app.get('/', (req, res) => {
+    res.send('News api running for')
+})
+
+app.listen(port, () => {
+    console.log(`Our port is listening${port}`)
+})
